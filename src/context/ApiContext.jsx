ad5008919c7ca5b1f592a937fe7e50ce452ff607
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-24 14:46:47 +0530

    commit - added custom hook to context

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,25 +1,27 @@
 import { createContext, useState } from "react";
 import axios from 'axios'
+import useForm from "../Hooks/useForm";
 
 
-export const ApiContext = createContext()
+export const ApiContext = createContext();
 
 export const ApiProvider = ({children}) =>{
+    const [cityValue , handleChange] = useForm("");
     const [cityName, setCityName] = useState()
-    const Base_URL = `https://api.weatherapi.com/v1/current.json?key=46a14caf5d5c4bde98c95253222512&q=${cityName}&aqi=yes`;
+    const Base_URL = `https://api.weatherapi.com/v1/current.json?key=46a14caf5d5c4bde98c95253222512&q=${cityValue}&aqi=yes`;
 
-  const FetchData = async() => {
+  const fetchData = async() => {
         try{
             const config = {headers: {Accept: 'application/json'}}
             const res = await axios.get(Base_URL,config)
-            const data =res.data
+            const data = res.data
          setCityName(data)
         }
         catch(e){
          console.log(e)
         }}
     
-    const value = {cityName, setCityName, FetchData}
+    const value = {cityName, fetchData, handleChange}
     
     return(
         <ApiContext.Provider value={value}>
