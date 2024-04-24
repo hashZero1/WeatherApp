a7fa655e1970742b08f277d81c09376383fcab0b
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2024-04-24 13:12:13 +0530

    commit- changes in ApiContext

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,26 +1,25 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import axios from 'axios'
 
 
 export const ApiContext = createContext()
 
 export const ApiProvider = ({children}) =>{
-    const [cityName, setCityName] = useState('')
+    const [cityName, setCityName] = useState()
+    const Base_URL = `https://api.weatherapi.com/v1/current.json?key=46a14caf5d5c4bde98c95253222512&q=${cityName}&aqi=yes`;
 
-  useEffect(() => {
-    async function FetchData(){
+  const FetchData = async() => {
         try{
-         const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=46a14caf5d5c4bde98c95253222512&q=${cityName}&aqi=yes`);
-         const data = response.data;
+            const config = {headers: {Accept: 'application/json'}}
+            const res = await axios.get(Base_URL,config)
+            const data =res.data
          setCityName(data)
         }
         catch(e){
          console.log(e)
         }}
-         FetchData()
-  },[cityName])
     
-    const value = {cityName, setCityName}
+    const value = {cityName, setCityName, FetchData}
     
     return(
         <ApiContext.Provider value={value}>
